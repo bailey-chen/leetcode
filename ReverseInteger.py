399066b03499014e397540c2d88e34ce17254c29
Bailey Chen <baileychen2019@gmail.com>
2018-09-07 12:46:56 -0700

    use non-data structures to pop/push when can

---
+++ ReverseInteger.py
@@ -0,0 +1,29 @@
+class Solution(object):
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # put digits (as strings) into list reverse order
+        # insert each digit at beginning of list
+        # convert list into integer using ''.join(lst):
+        neg_bound = (-1) * 2**31
+        pos_bound = 2**31 - 1
+
+        neg = False;
+        x_str = str(x)
+        if x_str[0] == '-':
+            neg = True;
+            x_str = x_str[1:]
+
+        lst = []
+        for c in x_str:
+            lst.insert(0, c)
+        if neg:
+            lst.insert(0, '-')
+        num = int(''.join(lst))
+        if num < neg_bound or num > pos_bound:
+            return 0
+        else:
+            return num
